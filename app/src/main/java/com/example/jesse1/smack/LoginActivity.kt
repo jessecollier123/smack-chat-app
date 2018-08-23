9fa43a5fa672bd7bfc9826ddd034e96212666153
Jesse Collier <jessecollier90@hotmail.com>
2018-08-22 21:19:06 -0400

    Created user login screen

---
+++ app/src/main/java/com/example/jesse1/smack/LoginActivity.kt
@@ -0,0 +1,22 @@
+package com.example.jesse1.smack
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun loginCreateUserButtonClicked(view: View) {
+
+    }
+
+    fun loginLoginButtonClicked(view: View) {
+
+    }
+
+}
--- app/src/main/java/com/example/jesse1/smack/MainActivity.kt
+++ app/src/main/java/com/example/jesse1/smack/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jesse1.smack
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
@@ -30,7 +31,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun loginButtonNavClicked(view: View){
-
+        val loginIntent = Intent(this, LoginActivity::class.java)
+        startActivity(loginIntent)
     }
 
     fun addChannelClicked(view: View){
@@ -38,6 +40,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendMsgBtnClicked(view: View) {
-        
+
     }
 }
